8f5686fe52fda4bedaa38ce3bcc1e997d65a22ac
Quozul <30729291+Quozul@users.noreply.github.com>
2025-08-25 14:00:15 +0200

    fix: Send correct version in KnownPacks

--- crates/minecraft_packets/src/configuration/data/known_pack.rs
+++ crates/minecraft_packets/src/configuration/data/known_pack.rs
@@ -2,17 +2,17 @@ use minecraft_protocol::prelude::*;
 
 #[derive(Debug, PacketIn, PacketOut)]
 pub struct KnownPack {
-    pub namespace: String,
-    pub id: String,
-    pub version: String,
+    namespace: String,
+    id: String,
+    version: String,
 }
 
-impl Default for KnownPack {
-    fn default() -> Self {
+impl KnownPack {
+    pub fn new(version: &str) -> Self {
         Self {
             namespace: "minecraft".to_string(),
             id: "core".to_string(),
-            version: "1.21.4".to_string(),
+            version: version.to_string(),
         }
     }
 }
