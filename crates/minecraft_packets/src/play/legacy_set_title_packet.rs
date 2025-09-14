3745ccecffdfd9c989afcde09603931207a2abc5
Axeno <tbavart@gmail.com>
2025-09-14 21:41:48 +0200

    feat: Enhance LegacySetTitlePacket with v1.11 action support and update related serialization

--- crates/minecraft_packets/src/play/legacy_set_title_packet.rs
+++ crates/minecraft_packets/src/play/legacy_set_title_packet.rs
@@ -3,13 +3,26 @@ use pico_text_component::prelude::Component;
 
 #[derive(PacketOut)]
 pub struct LegacySetTitlePacket {
+    #[pvn(315..)]
     action: LegacySetTitleAction,
+    #[pvn(..315)]
+    v1_11_action: LegacyV1_11SetTitleAction,
 }
 
 impl LegacySetTitlePacket {
+    fn new(title_action: LegacySetTitleAction, v1_11_action: LegacyV1_11SetTitleAction) -> Self {
+        Self {
+            action: title_action,
+            v1_11_action,
+        }
+    }
+
     pub fn set_title(title: &Component) -> Self {
         Self {
             action: LegacySetTitleAction::SetTitle {
+                title: title.clone(),
+            },
+            v1_11_action: LegacyV1_11SetTitleAction::SetTitle {
                 title: title.clone(),
             },
         }
@@ -20,20 +33,20 @@ impl LegacySetTitlePacket {
             action: LegacySetTitleAction::SetSubtitle {
                 subtitle: subtitle.clone(),
             },
-        }
-    }
-
-    pub fn set_action_bar(action_bar: &Component) -> Self {
-        Self {
-            action: LegacySetTitleAction::SetActionBar {
-                action_bar: action_bar.clone(),
+            v1_11_action: LegacyV1_11SetTitleAction::SetSubtitle {
+                subtitle: subtitle.clone(),
             },
         }
     }
 
-    pub fn set_times_and_display(fade_in: i32, stay: i32, fade_out: i32) -> Self {
+    pub fn set_animation(fade_in: i32, stay: i32, fade_out: i32) -> Self {
         Self {
             action: LegacySetTitleAction::SetTimesAndDisplay {
+                fade_in,
+                stay,
+                fade_out,
+            },
+            v1_11_action: LegacyV1_11SetTitleAction::SetTimesAndDisplay {
                 fade_in,
                 stay,
                 fade_out,
@@ -44,12 +57,23 @@ impl LegacySetTitlePacket {
     pub fn hide() -> Self {
         Self {
             action: LegacySetTitleAction::Hide {},
+            v1_11_action: LegacyV1_11SetTitleAction::Hide {},
         }
     }
 
     pub fn reset() -> Self {
         Self {
             action: LegacySetTitleAction::Reset {},
+            v1_11_action: LegacyV1_11SetTitleAction::Reset {},
+        }
+    }
+
+    pub fn action_bar(action_bar: &Component) -> Self {
+        Self {
+            action: LegacySetTitleAction::SetActionBar {
+                action_bar: action_bar.clone(),
+            },
+            v1_11_action: LegacyV1_11SetTitleAction::Hide {}
         }
     }
 
@@ -61,9 +85,9 @@ impl LegacySetTitlePacket {
         fade_out: i32,
     ) -> Vec<Self> {
         vec![
-            Self::set_times_and_display(fade_in, stay, fade_out),
             Self::set_title(title),
             Self::set_subtitle(subtitle),
+            Self::set_animation(fade_in, stay, fade_out),
         ]
     }
 }
@@ -88,6 +112,22 @@ enum LegacySetTitleAction {
     Reset {},
 }
 
+enum LegacyV1_11SetTitleAction {
+    SetTitle {
+        title: Component,
+    },
+    SetSubtitle {
+        subtitle: Component,
+    },
+    SetTimesAndDisplay {
+        fade_in: i32,
+        stay: i32,
+        fade_out: i32,
+    },
+    Hide {},
+    Reset {},
+}
+
 impl LegacySetTitleAction {
     fn type_id(&self) -> u8 {
         match self {
@@ -97,6 +137,18 @@ impl LegacySetTitleAction {
             LegacySetTitleAction::SetTimesAndDisplay { .. } => 3,
             LegacySetTitleAction::Hide {} => 4,
             LegacySetTitleAction::Reset {} => 5,
+        }
+    }
+}
+
+impl LegacyV1_11SetTitleAction {
+    fn type_id(&self) -> u8 {
+        match self {
+            LegacyV1_11SetTitleAction::SetTitle { .. } => 0,
+            LegacyV1_11SetTitleAction::SetSubtitle { .. } => 1,
+            LegacyV1_11SetTitleAction::SetTimesAndDisplay { .. } => 2,
+            LegacyV1_11SetTitleAction::Hide {} => 3,
+            LegacyV1_11SetTitleAction::Reset {} => 4,
         }
     }
 }
@@ -131,6 +183,40 @@ impl EncodePacket for LegacySetTitleAction {
                 // Nothing to encode
             }
             LegacySetTitleAction::Reset {} => {
+                // Nothing to encode
+            }
+        }
+        Ok(())
+    }
+}
+
+impl EncodePacket for LegacyV1_11SetTitleAction {
+    fn encode(
+        &self,
+        writer: &mut BinaryWriter,
+        protocol_version: ProtocolVersion,
+    ) -> Result<(), BinaryWriterError> {
+        self.type_id().encode(writer, protocol_version)?;
+        match self {
+            LegacyV1_11SetTitleAction::SetTitle { title } => {
+                title.encode(writer, protocol_version)?;
+            }
+            LegacyV1_11SetTitleAction::SetSubtitle { subtitle } => {
+                subtitle.encode(writer, protocol_version)?;
+            }
+            LegacyV1_11SetTitleAction::SetTimesAndDisplay {
+                fade_in,
+                stay,
+                fade_out,
+            } => {
+                fade_in.encode(writer, protocol_version)?;
+                stay.encode(writer, protocol_version)?;
+                fade_out.encode(writer, protocol_version)?;
+            }
+            LegacyV1_11SetTitleAction::Hide {} => {
+                // Nothing to encode
+            }
+            LegacyV1_11SetTitleAction::Reset {} => {
                 // Nothing to encode
             }
         }
--- pico_limbo/src/server/packet_registry.rs
+++ pico_limbo/src/server/packet_registry.rs
@@ -23,6 +23,7 @@ use minecraft_packets::play::commands_packet::CommandsPacket;
 use minecraft_packets::play::disconnect_packet::DisconnectPacket;
 use minecraft_packets::play::game_event_packet::GameEventPacket;
 use minecraft_packets::play::legacy_chat_message_packet::LegacyChatMessagePacket;
+use minecraft_packets::play::legacy_set_title_packet::LegacySetTitlePacket;
 use minecraft_packets::play::login_packet::LoginPacket;
 use minecraft_packets::play::play_client_bound_plugin_message_packet::PlayClientBoundPluginMessagePacket;
 use minecraft_packets::play::player_info_update_packet::PlayerInfoUpdatePacket;
@@ -31,7 +32,6 @@ use minecraft_packets::play::set_default_spawn_position_packet::SetDefaultSpawnP
 use minecraft_packets::play::set_entity_data_packet::SetEntityMetadataPacket;
 use minecraft_packets::play::set_player_position_and_rotation_packet::SetPlayerPositionAndRotationPacket;
 use minecraft_packets::play::set_player_position_packet::SetPlayerPositionPacket;
-use minecraft_packets::play::legacy_set_title_packet::LegacySetTitlePacket;
 use minecraft_packets::play::set_title_text_packet::SetTitleTextPacket;
 use minecraft_packets::play::set_titles_animation::SetTitlesAnimationPacket;
 use minecraft_packets::play::synchronize_player_position_packet::SynchronizePlayerPositionPacket;
@@ -295,7 +295,11 @@ pub enum PacketRegistry {
     )]
     SetSubtitleText(SetTitleTextPacket),
 
-    #[protocol_id(state = "play", bound = "clientbound", name = "minecraft:legacy_set_title")]
+    #[protocol_id(
+        state = "play",
+        bound = "clientbound",
+        name = "minecraft:legacy_set_title"
+    )]
     LegacySetTitle(LegacySetTitlePacket),
 }
 
