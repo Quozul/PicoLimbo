b53bcdf89eb452fe8fb3a521fc6f9ffa2f993937
Quozul <30729291+Quozul@users.noreply.github.com>
2025-09-17 17:43:13 +0200

    chore: Add registries and blocks reports for 1.16-1.18.2

--- crates/blocks_report/build/blocks_report_loader.rs
+++ crates/blocks_report/build/blocks_report_loader.rs
@@ -59,7 +59,7 @@ pub fn load_block_data() -> anyhow::Result<Vec<BlocksReport>> {
             ProtocolVersion::from_str(&name)
                 .ok()
                 .and_then(|protocol_version| {
-                    if protocol_version.is_after_inclusive(ProtocolVersion::V1_19) {
+                    if protocol_version.is_after_inclusive(ProtocolVersion::V1_16) {
                         let version_path = entry.path();
                         let blocks_report_path = version_path.join("reports").join("blocks.json");
                         println!("cargo:rerun-if-changed={}", blocks_report_path.display());
