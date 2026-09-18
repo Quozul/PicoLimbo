dc4875c810ad81de3f475fe541eaa5ce57db8d70
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-18 07:41:07 -0400

    Simplify Floodgate configuration

--- pico_limbo/src/configuration/floodgate.rs
+++ pico_limbo/src/configuration/floodgate.rs
@@ -1,40 +1,23 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-#[serde(tag = "method", rename_all = "SCREAMING_SNAKE_CASE")]
-pub enum FloodgateConfig {
-    #[serde(alias = "disabled")]
-    Disabled,
-
-    #[serde(alias = "enabled")]
-    Enabled {
-        key_file: String,
-        username_prefix: String,
-        replace_spaces: bool,
-        education: EducationConfig,
-    },
+#[serde(default)]
+pub struct FloodgateConfig {
+    pub enabled: bool,
+    pub key_file: String,
+    pub username_prefix: String,
+    pub replace_spaces: bool,
+    pub education: bool,
 }
 
 impl Default for FloodgateConfig {
     fn default() -> Self {
-        Self::Disabled
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(default)]
-pub struct EducationConfig {
-    pub enabled: bool,
-    pub username_prefix: String,
-    pub uuid_legacy: bool,
-}
-
-impl Default for EducationConfig {
-    fn default() -> Self {
         Self {
             enabled: false,
-            username_prefix: "+".into(),
-            uuid_legacy: false,
+            key_file: "key.pem".into(),
+            username_prefix: ".".into(),
+            replace_spaces: true,
+            education: false,
         }
     }
 }
