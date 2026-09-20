d2cee9b8be6d671be9475fe50a3e10240196a44c
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-20 17:39:38 -0400

    Fix Floodgate config defaults and generation

--- pico_limbo/src/configuration/floodgate.rs
+++ pico_limbo/src/configuration/floodgate.rs
@@ -4,8 +4,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum FloodgateConfig {
-    Enabled(EnabledFloodgateConfig),
     Disabled(DisabledFloodgateConfig),
+    Enabled(EnabledFloodgateConfig),
 }
 
 #[derive(Deserialize, Serialize)]
@@ -13,22 +13,65 @@ pub enum FloodgateConfig {
 pub struct EnabledFloodgateConfig {
     #[serde(deserialize_with = "require_true")]
     pub enabled: bool,
+    #[serde(default = "default_key_file")]
     pub key_file: String,
+    #[serde(default = "default_username_prefix")]
     pub username_prefix: String,
+    #[serde(default = "default_replace_spaces")]
     pub replace_spaces: bool,
+    #[serde(default)]
     pub education: bool,
+    #[serde(default = "default_education_username_prefix")]
     pub education_username_prefix: String,
+    #[serde(default)]
     pub education_uuid_legacy: bool,
 }
 
 #[derive(Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 pub struct DisabledFloodgateConfig {
     #[serde(deserialize_with = "require_false")]
     pub enabled: bool,
+    #[serde(default = "default_key_file")]
+    pub key_file: String,
+    #[serde(default = "default_username_prefix")]
+    pub username_prefix: String,
+    #[serde(default = "default_replace_spaces")]
+    pub replace_spaces: bool,
+    #[serde(default)]
+    pub education: bool,
+    #[serde(default = "default_education_username_prefix")]
+    pub education_username_prefix: String,
+    #[serde(default)]
+    pub education_uuid_legacy: bool,
+}
+
+fn default_key_file() -> String {
+    "key.pem".to_string()
+}
+
+fn default_username_prefix() -> String {
+    ".".to_string()
+}
+
+fn default_replace_spaces() -> bool {
+    true
+}
+
+fn default_education_username_prefix() -> String {
+    "+".to_string()
 }
 
 impl Default for FloodgateConfig {
     fn default() -> Self {
-        Self::Disabled(DisabledFloodgateConfig { enabled: false })
+        Self::Disabled(DisabledFloodgateConfig {
+            enabled: false,
+            key_file: default_key_file(),
+            username_prefix: default_username_prefix(),
+            replace_spaces: default_replace_spaces(),
+            education: false,
+            education_username_prefix: default_education_username_prefix(),
+            education_uuid_legacy: false,
+        })
     }
 }
